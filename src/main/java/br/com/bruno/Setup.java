3993565b488198ba1340808104b28b41c94a5306
brunojdsz <bruno.bjwm@gmail.com>
2022-10-04 00:19:23 -0300

    First Step in Java with Spring boot 3

---
+++ src/main/java/br/com/bruno/Setup.java
@@ -0,0 +1,13 @@
+package br.com.bruno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Setup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Setup.class, args);
+	}
+
+}
